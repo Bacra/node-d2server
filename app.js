dd01b0ec5d6cecacaf900a13cea383510d48ed16
Bacra Woo <wf@tianqu.com.cn>
2013-05-22 17:17:39 +0800

    重大调整： 1、两个server的端口可配置 2、完善io服务端和fs watch事件，增加刷新功能 3、完善parseHTML方法，增加拓展tpl处理 4、删除LastModified判断

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var notice = require('./lib/notice.js'),
-	fileServ = require('./lib/module/fileserv.js'),
-	infoServ = require('./lib/module/infoserv.js'),
-	rl = require('./lib/module/readcmd.js');
+	rl = require('./lib/module/readcmd.js'),
+	_conf = require('./lib/conf.js');
 
-fileServ.listen(82);
-infoServ.listen(81);
-notice.log('INFO',  'Server run in Port:82');
+
+
+require('./lib/module/fileserv.js').listen(_conf.fileServPort);
+require('./lib/module/ioserv.js');
+require('./lib/module/infoserv.js').listen(_conf.infoServPort);
+notice.log('INFO',  'File Server run in Port: '+_conf.fileServPort);
+notice.log('INFO',  'IO Server run in Port: '+_conf.infoServPort);
