86689a4af392222b907e8afd7ba77dc47dd3a2f7
Bacra Woo <wf@tianqu.com.cn>
2013-06-05 09:53:50 +0800

    serv启动时，增加端口被占用的错误提示

--- serv.js
+++ serv.js
@@ -1,19 +1,26 @@
 var notice = require('./lib/notice.js'),
 	rl = require('./lib/apps/readcmd.js'),
-	_conf = require('./conf.js');
+	_conf = require('./conf.js'),
 
+	devServ = require('./lib/apps/devserv.js'),
+	downServ = require('./lib/apps/downserv.js'),
+	infoServ = require('./lib/apps/infoserv.js'),
+	viewServ = require('./lib/apps/viewserv.js'),
+	spliceServ = require('./lib/apps/spliceserv.js'),
 
+	listen = function(serv, port, name){
+		try {
+			serv.listen(port);
+			notice.log('SYS', name+' run in Port:'+port);
+		} catch (err) {
+			notice.error('SYS', name+' '+port+'Port is Occupied');
+		}
+	};
 
-
-require('./lib/apps/devserv.js').listen(_conf.DevServPort);
-require('./lib/apps/downserv.js');
-require('./lib/apps/infoserv.js').listen(_conf.InfoServPort);
-require('./lib/apps/viewserv.js').listen(_conf.ViewServPort);
-require('./lib/apps/spliceserv.js').listen(_conf.SpliceServPort);
-notice.log('INFO',  'Dev Server run in Port: '+_conf.DevServPort);
-notice.log('INFO',  'Info Server run in Port: '+_conf.InfoServPort);
-notice.log('INFO',  'View Server run in Port: '+_conf.ViewServPort);
-notice.log('INFO',  'Splice Server run in Port: '+_conf.SpliceServPort);
+listen(devServ, _conf.DevServPort, 'Dev Server');
+listen(infoServ, _conf.InfoServPort, 'Info Server');
+listen(viewServ, _conf.ViewServPort, 'View Server');
+listen(spliceServ, _conf.SpliceServPort, 'Splice Server');
 
 
 setTimeout(function(){
