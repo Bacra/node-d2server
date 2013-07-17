17db34319d9d5d7ca3d71f3c9a07afb12bff4c19
Bacra Woo <Bacra.Woo@gmail.com>
2013-07-18 01:04:11 +0800

    新版dataAPI基本轮廓（未完成）
    注意：和旧版差别极大：
    1、不仅配置不同（配置方案中，不再是函数，而是简单配置几个参数）
    2、接口不是在server最前面调用，而准备移动到最末尾，和404处理函数同级
    3、通过wget，可自动下载线上数据
    4、暂时不提供step方法

--- conf.js
+++ conf.js
@@ -2,12 +2,13 @@ var SourcePath = ".source/";
 
 module.exports = {
 	'DocumentRoot': __dirname + '/wwwroot/',				// 所有项目所在根目录
-	'Domain': 'test.com',						// 绑定的顶级域名
-	'SourcePath': SourcePath,					// 项目开发文件存放文件夹
-	'MainConfigFile': SourcePath + 'ProjConf.js',	// 项目主体配置文件
-	'ProjConfigFile': SourcePath + 'wf_ProjConf.js',	// 项目成员各自的配置文件
-	'DynamicDataPath': SourcePath + '.data/',		// 存放动态数据的目录
-	'StyleMapFile': SourcePath+'.stylemap',			// 存放样式解析后的映射关系
+	'Domain': 'test.com',									// 绑定的顶级域名
+	'SourcePath': SourcePath,								// 项目开发文件存放文件夹
+	'MainConfigFile': SourcePath + 'ProjConf.js',			// 项目主体配置文件
+	'ProjConfigFile': SourcePath + 'wf_ProjConf.js',		// 项目成员各自的配置文件
+	'DynamicDataPath': SourcePath + '.data/',				// 存放动态数据的目录
+	'DynamicDataFile': SourcePath + '.data/.fileinfo',		// 存放动态数据的配置方案
+	'StyleMapFile': SourcePath+'.stylemap',					// 存放样式解析后的映射关系
 	'DevServPort': 82,				// 开发服务器端口
 	'InfoServPort': 81,				// 信息服务器端口
 	'ViewServPort': 80,				// 静态文件浏览服务器端口
