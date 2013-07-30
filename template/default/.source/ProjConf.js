b3a938b8840635effa2614a4abaee2af8f8cdf0d
Bacra Woo <Bacra.Woo@gmail.com>
2013-07-30 10:28:07 +0800

    项目配置文件增加catalog参数，简化项目相对于域名的内嵌目录

--- template/default/.source/ProjConf.js
+++ template/default/.source/ProjConf.js
@@ -1,6 +1,8 @@
 module.exports = {
 	"name": "{@projRoot}",							// 项目名称
 	"alias": "{@projRoot}",							// 项目的默认二级域名
+	"sync": false,									// 同步目录
+	"catalog": false,								// 项目内嵌的二级目录
 	"baseLess": "common/b.less",					// less公共文件
 	"MinCssName": false,							// 压缩样式名的前缀
 	"hostname": false,								// 线上资源域名
