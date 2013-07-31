a6f0a7d791807f2989678cf4776289dca7ef829f
Bacra Woo <Bacra.Woo@gmail.com>
2013-07-31 20:27:44 +0800

    修正convertSource4HTML方法，从配置文件入手，fileMap的文件路径参照项目根目录，而非html文件中的file路径
    额外的优化：
    1. 及时各个子配置文件中fileMap的文件路径定义不同，只要解析结果一致，依然会被合并

--- template/default/.source/ProjConf.js
+++ template/default/.source/ProjConf.js
@@ -18,23 +18,23 @@ module.exports = {
 		}
 	},
 	"fileMap": {						// 配置文件映射（公共部分）
-		"/css/base.min.css": [
+		"css/base.min.css": [
 			'common/reset.css',
 			'common/base.css',
 			'common/nav.less',
 			'common/frame.less'
 		],
-		"/css/page.min.css": [
+		"css/page.min.css": [
 			"index/index.less"
 		],
 
-		"/js/base.min.js": [
+		"js/base.min.js": [
 			'common/nav.js',
 			'common/nav.test.js',
 			'common/frame.js',
 			'common/frame.test.js'
 		],
-		"/js/page.min.js": [
+		"js/page.min.js": [
 			"index/index.js",
 			"index/index.test.js"
 		]
