bd328d0738784187df31de2914c172832e20eb3a
Bacra Woo <Bacra.Woo@gmail.com>
2013-09-02 00:05:15 +0800

    Add：配置文件增加中间层，处理package中的信息

--- serv.js
+++ serv.js
@@ -1,19 +1,15 @@
 var notice = require('./lib/func/notice.js');
 
-if (require('fs').existsSync('./conf.js')){
-	var _conf = require('./conf.js');
+var _conf = require('./lib/config.js');
 
-	listen('devServ', _conf.DevServPort, 'Dev Server');
-	listen('infoServ', _conf.InfoServPort, 'Info Server');
-	listen('viewServ', _conf.ViewServPort, 'View Server');
-	listen('spliceServ', _conf.SpliceServPort, 'Splice Server');
+listen('devServ', _conf.DevServPort, 'Dev Server');
+listen('infoServ', _conf.InfoServPort, 'Info Server');
+listen('viewServ', _conf.ViewServPort, 'View Server');
+listen('spliceServ', _conf.SpliceServPort, 'Splice Server');
 
-	// plugs
-	require('./lib/module/downServ/downServ.js');
-	require('./lib/module/cmd/cmd.js');
-} else {
-	notice.error('SYS', 'Please edit "'+__dirname+require('path').sep+'conf.js.example" file and creates "conf.js" file');
-}
+// plugins
+require('./lib/module/downServ/downServ.js');
+require('./lib/module/cmd/cmd.js');
 
 
 
