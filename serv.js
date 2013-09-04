47caf5e327095d19a627447fe7a05067592acae1
Bacra Woo <Bacra.Woo@gmail.com>
2013-09-04 12:45:02 +0800

    Add：分离runProjConfig和ProjConfig，将初始化分离出来，方便在插件加载完成之后再初始化项目信息 Add：在serv.js中增加相应的初始化msg

--- serv.js
+++ serv.js
@@ -1,17 +1,28 @@
 var notice = require('./lib/func/notice.js'),
 	_conf = require('./lib/config.js');
 
+notice.log('SYS', 'start servers');
 listen('devServ', _conf.DevServPort, 'Dev Server');
 listen('infoServ', _conf.InfoServPort, 'Info Server');
 listen('viewServ', _conf.ViewServPort, 'View Server');
 listen('spliceServ', _conf.SpliceServPort, 'Splice Server');
 require('./lib/module/cmd/cmd.js');
 
+
 // 加载plugins
+notice.log('SYS', 'start load plugins');
 require('fs').readdirSync(__dirname+'/lib/plugins/').forEach(function(dirname){
-	if (dirname != '.'&& dirname != '..') require('./lib/plugins/'+dirname+'/'+dirname+'.js');
+	if (dirname != '.'&& dirname != '..') {
+		require('./lib/plugins/'+dirname+'/'+dirname+'.js');
+		notice.log('SYS', 'load '+dirname+' plugin successs');
+	}
 });
 
+
+
+// 读取配置文件
+notice.log('SYS', 'start load Project Config');
+require('./lib/module/ProjConfig/runProjConfig.js');
 
 
 function listen(serv, port, name){
